03707a83627be7c41e0ca4521cefa97e5c09a19f
CCaravita <caravita.clement@gmail.com>
2026-01-29 21:49:55 +0100

    move python files to src directory

---
+++ src/yt_audio_downloader.py
@@ -0,0 +1,22 @@
+from yt_dlp import YoutubeDL
+import time_method
+
+def download_audio(url: str) -> str:
+    with time_method.timed("download_audio"):
+        options = {
+            "format": "bestaudio/best",
+            "outtmpl": "audios/%(title)s.%(ext)s",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "wav"
+                }
+            ],
+        }
+
+        with YoutubeDL(options) as ydl:
+            ydl.download([url])
+            info = ydl.extract_info(url, download=True)
+            filepath = info["requested_downloads"][0]["filepath"]
+
+    return filepath
