781564ddebe3a6d6c8ee7986cd95d80b462f6905
CCaravita <caravita.clement@gmail.com>
2026-05-09 23:30:23 +0200

    audio_video_converter.py small changes

--- src/audio_video_converter.py
+++ src/audio_video_converter.py
@@ -19,3 +19,22 @@ def webm_to_mp3(input_path):
     )
 
     return output_path
+
+
+def video_to_wav(input_path, output_suffix=".wav"):
+    input_path = Path(input_path)
+    output_path = input_path.with_suffix(output_suffix)
+
+    subprocess.run(
+        [
+            "ffmpeg",
+            "-y",
+            "-i", str(input_path),
+            "-vn",
+            "-acodec", "pcm_s16le",
+            str(output_path)
+        ],
+        check=True,
+    )
+
+    return output_path
